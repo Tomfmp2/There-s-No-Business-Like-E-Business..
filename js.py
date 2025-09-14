00c3c6d9bf0001a2bbc84fb25cc5ebf6bc4442bc
Tomfmp2 <222234754+Tomfmp2@users.noreply.github.com>
2025-09-14 17:03:40 -0500

    Fix :adhesive_bandage: Unnecessary code bloke removed

--- js.py
+++ js.py
@@ -8,14 +8,4 @@ JSON_CUENTAS = "data/JsonCuentas.json"
 def write_json(file_path: str, data: Dict) -> None:
     with open(file_path, "w", encoding='utf-8') as file:
         json.dump(data, file, indent=4)
-
-darta = {"hola": "saludo"}
-
-crearJs = input('Desea crear el archivo? (s/n).').lower()
-match crearJs:
-    case "n":
-        print('okey..')
-    case "s":
-        write_json(JSON_CUENTAS, darta)
-    case _:
-        print('El dato ingresado no esta disponible como opcion..')
+        
