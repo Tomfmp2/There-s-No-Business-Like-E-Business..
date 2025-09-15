cf86750a8c3504c658c8d5d8ac24f61f21349227
Sahiam1810 <222233860+Sahiam1810@users.noreply.github.com>
2025-09-14 19:47:40 -0500

    feat: :sparkles: Create branch featue-fornt and add menus.py
    Create branch featue-fornt and add menus.py

---
+++ menus.py
@@ -0,0 +1,51 @@
+def vistaprincipal():
+
+    print("""
+    -----------------------------------
+    -------Bienvenido al sistema-------
+    -----------------------------------""")
+
+    opcion = int(input(
+        
+        """
+        -------------------------
+        ____Elige una opcion:____
+        -------------------------
+
+        _________________________
+                                 *
+        --1 Resgistrarme         *
+        --2 Iniciar sesion       *
+        _________________________*
+
+
+        """))
+
+            match opcion:
+                case 1:
+                    pass
+
+            match opcion:
+                case 2:
+                    pass
+        
+
+
+def vistaCuentaCreada():
+
+    print("""
+    _________________________
+
+    -----¡Bienvenido!--------
+    _________________________
+    
+    """)
+
+def 
+
+
+
+
+
+
+
